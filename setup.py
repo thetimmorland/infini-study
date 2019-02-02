cc0468ca6ed1ab8de70c36d746c80cd724eed285
Mitchell Ottaway <mitchello288@gmail.com>
2019-02-02 10:13:25 -0500

    Added name

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
         name='infini-study',
         version='0.1',
         description='Practice problem generator and scheduler',
-        author='Timothy Morland',
-        author_email='thetimmorland@gmail.com',
+        author='Timothy Morland, Mitchell Ottaway',
+        author_email='thetimmorland@gmail.com, mitchello288@gmail.com',
         url='https://github.com/thetimmorland/infini-study',
         packages=['infini-study'],
         install_requires=[

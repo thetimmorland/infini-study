9534cb7cac7be7f14a1cd462eaee65431019e3ed
Timothy John Morland <thetimmorland@gmail.com>
2019-02-02 09:31:09 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='infini-study',
+        version='0.1',
+        description='Practice problem generator and scheduler',
+        author='Timothy Morland',
+        author_email='thetimmorland@gmail.com',
+        url='https://github.com/thetimmorland/infini-study',
+        packages=['infini-study'],
+        )
+

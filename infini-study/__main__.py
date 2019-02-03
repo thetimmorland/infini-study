5aee57923727649c55648448ea6f7e378983919f
Timothy John Morland <thetimmorland@gmail.com>
2019-02-03 01:28:50 -0500

    rip mitch

--- infini-study/__main__.py
+++ infini-study/__main__.py
@@ -1,15 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-import PyQt5.QtWebEngineWidgets
-from ui import mainWindow, jupyterView
+
+from PyQt5.QtWidgets import QApplication
+from mainWindow import mainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = mainWindow()
     win.show()
-
-    view = jupyterView()
-    view.show()
-
     sys.exit(app.exec_())
         
